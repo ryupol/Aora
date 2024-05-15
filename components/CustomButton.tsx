3d81ad8b3f21e1e54265f8267a67a1e8bcca6138
Ryu <ryupol@hotmail.com>
2024-05-16 00:26:23 +0700

    'create initial, sign in & signup pages'

---
+++ components/CustomButton.tsx
@@ -0,0 +1,35 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+type ButtonProps = {
+  title: string;
+  handlePress: () => void;
+  containerStyles?: string;
+  textStyles?: string;
+  isLoading?: boolean;
+};
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
